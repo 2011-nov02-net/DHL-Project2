0d24ea93f0ad68fba610b0c242652ad603dc02dc
harelfishbein <harelfishbein@github.com>
2020-12-14 13:59:37 -0800

    connect to DHLProject2SchoolContext

--- Project2.Api/Startup.cs
+++ Project2.Api/Startup.cs
@@ -1,11 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+// using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,14 +14,21 @@ namespace Project2.Api
 {
     public class Startup
     {
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Environment = env;
+            Configuration = configuration;
+        }
         /// <summary>
         /// This method gets called by the runtime. 
         /// Use this method to add services to the container.
         /// </summary>
         public void ConfigureServices(IServiceCollection services)
         {
-            // db set up
-            // services.AddDbContext<Project2Context>(options => options.UseSqlServer(Configuration.GetConnectionString("Project2connection")));
+            services.AddDbContext<DHLProject2SchoolContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("Project2connection")));
             services.AddControllers();
         }
 
