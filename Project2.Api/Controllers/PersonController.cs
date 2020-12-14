75dfad6cb2b7e017e70da93dc0172f3d8501cae9
harelfishbein <harelfishbein@github.com>
2020-12-14 14:26:45 -0800

    added module dependency

--- Project2.Api/Controllers/PersonController.cs
+++ Project2.Api/Controllers/PersonController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Project2.DataModel;
 
 namespace Project2.Api.Controllers
 {
