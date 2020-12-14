74d17725ef6b5f908bd169d986ea2ae88fcbe451
harelfishbein <harelfishbein@github.com>
2020-12-14 10:00:22 -0800

    add a version of personController without repos

---
+++ Project2.Api/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly DbSet<Person> _personRepository;
+        public PersonController(DbSet<Person> personRepository)
+        {
+            _personRepository = personRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {            
+            return Ok(await _personRepository.ToListAsync() );
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            if (await _personRepository.FirstOrDefaultAsync(u => u.Id == id) 
+                    is Person person) return Ok(person);
+            return NotFound();
+        }
+    }
+}
