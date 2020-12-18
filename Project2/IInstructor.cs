d157559b6c27955b7f0e5c69cfcacd7a74d7e3fb
harelfishbein <harelfishbein@github.com>
2020-12-18 14:35:00 -0800

    rename Instructor interface file

---
+++ Project2/IInstructor.cs
@@ -0,0 +1,10 @@
+﻿namespace Project2
+{
+    public interface IInstructor
+    {
+        int InstructorId { get; set; }
+        int CourseId { get; set; }
+        ICourse Course { get; set; }
+        IUser InstructorNavigation { get; set; }
+    }
+}
