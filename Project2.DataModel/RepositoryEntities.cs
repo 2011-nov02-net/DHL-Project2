3a4684befd9f9f179a674656ef44050ad039b4a3
harelfishbein <harelfishbein@github.com>
2020-12-18 13:48:37 -0800

    Moved repository & added dependency to DataModel

--- Project2.DataModel/RepositoryEntities.cs
+++ Project2.DataModel/RepositoryEntities.cs
@@ -9,14 +9,7 @@ using Microsoft.EntityFrameworkCore.Query;
 
 namespace Project2.DataModel
 {
-    public interface IRepository<T> : IQueryable<T>, ICollection<T> { 
-        public void Update(T item);
-        public ValueTask<T> FindAsync(object keyValue);
-        public ValueTask<bool> AddAsync(T item);
-        public ValueTask<bool> RemoveAsync(T item);
-        public Task<object> UpdateAsync(T item);
-    }
-    public class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity<int>
+    public class Repository<TEntity> : IRepositoryAsync<TEntity> where TEntity : EntityBase
     {
         private readonly DHLProject2SchoolContext _context;
         private readonly DbSet<TEntity> _dbSet;
@@ -80,64 +73,4 @@ namespace Project2.DataModel
             return await _context.SaveChangesAsync();
         }
     }
-    // https://www.c-sharpcorner.com/article/generic-repository-pattern-in-asp-net-core/
-    public class Entity<PkT> {
-        public PkT Id {get; set;}
-    }
-    partial class Class : Entity<int> { }
-    partial class Person : Entity<int> { }
-    partial class Building : Entity<int> { }
-    partial class Department : Entity<int> { }
-    /*
-    public abstract class Repository<T> : IQueryable<T>, ICollection<T>
-    {
-        public Type ElementType {get; protected set;}
-        public Expression Expression {get; protected set;}
-        public IQueryProvider Provider {get; protected set;}
-        public abstract int Count { get; }
-        public abstract bool IsReadOnly { get; }
-
-        public abstract IEnumerator<T> GetEnumerator();
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-        public abstract void Add(T x);
-        public abstract void Remove(T x);
-        public abstract void Clear();
-        public abstract bool Contains(T item);
-        public abstract void CopyTo(T[] array, int arrayIndex);
-
-        bool ICollection<T>.Remove(T item)
-        {
-            throw new NotImplementedException();
-        }
-    }
-    public class RepositoryEF<TEntity> : Repository<TEntity> where TEntity : Entity<int>
-    {
-        private readonly DHLProject2SchoolContext _context;
-        private readonly DbSet<TEntity> _dbSet;
-        public RepositoryEF(DHLProject2SchoolContext context, DbSet<TEntity> dbSet)
-        {
-            _context = context;
-            _dbSet = dbSet;
-            ElementType = _dbSet.AsQueryable().ElementType;
-            Expression = _dbSet.AsQueryable().Expression;
-            Provider = _dbSet.AsQueryable().Provider;
-        }
-        public override IEnumerator<TEntity> GetEnumerator() => _dbSet.AsQueryable().GetEnumerator();
-        public override void Add(TEntity x)
-        {
-            _dbSet.Add(x);
-            _context.SaveChanges();
-        }
-        public async void AddAsync(TEntity x)
-        {
-            await _dbSet.AddAsync(x);
-            await _context.SaveChangesAsync();
-        }
-        public override void Remove(TEntity x)
-        {
-            _dbSet.Remove(x);
-            _context.SaveChanges();
-        }
-    }
-    */
 }
